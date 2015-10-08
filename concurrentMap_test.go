cad0dc86166d68aeb70420cd67c69ef0f18243b5
Charlesworth <cochrane26@gmail.com>
2015-10-08 23:23:46 +0100

    split concurrent map into its own file and test file

---
+++ concurrentMap_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"testing"
+)
+
+var testMap *concurrentMap
+
+func BeforeEach() {
+	testMap = new()
+
+	testMap.m["zero"] = 0
+	testMap.m["one"] = 1
+}
+
+func TestConcurrentMapGet(t *testing.T) {
+	BeforeEach()
+	if testMap.get("zero") != 0 {
+		t.Error("Get on map['zero'] did not return 0")
+	}
+
+	if testMap.get("one") != 1 {
+		t.Error("Get on map['one'] did not return 1")
+	}
+}
+
+func TestConcurrentMapSet(t *testing.T) {
+	BeforeEach()
+	testMap.set("test", 66)
+	if testMap.get("test") != 66 {
+		t.Error("Set on previosly un-initialized map['test'] of 66 did not return 66 on a get")
+	}
+
+	testMap.set("test", 22)
+	if testMap.get("test") != 22 {
+		t.Error("Set on already initialized map['test'] of 22 did not return 22 on a get")
+	}
+}
+
+func TestConcurrentMapIncrement(t *testing.T) {
+	BeforeEach()
+	if testMap.increment("zero") != 1 {
+		t.Error("Increment on map['zero']  did not return 1")
+	}
+
+	if testMap.increment("one") != 2 {
+		t.Error("Increment on map['one'] did not return 2")
+	}
+}
+
+func TestGetMap(t *testing.T) {
+	BeforeEach()
+	exportedMap := testMap.getMap()
+	if exportedMap["zero"] != 0 {
+		t.Error("getMap did not return the initialized map")
+	}
+}
--- goCounter.go
+++ goCounter.go
@@ -2,44 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
 	fmt.Println("hi")
-}
-
-type concurrentMap struct {
-	m map[string]int
-	*sync.RWMutex
-}
-
-func new() *concurrentMap {
-	return &concurrentMap{make(map[string]int), &sync.RWMutex{}}
-}
-
-func (concurrentMap *concurrentMap) set(name string, count int) {
-	concurrentMap.Lock()
-	concurrentMap.m[name] = count
-	concurrentMap.Unlock()
-	return
-}
-
-func (concurrentMap *concurrentMap) get(name string) (count int) {
-	concurrentMap.RLock()
-	count = concurrentMap.m[name]
-	concurrentMap.RUnlock()
-	return count
-}
-
-func (concurrentMap *concurrentMap) increment(name string) (count int) {
-	concurrentMap.Lock()
-	concurrentMap.m[name]++
-	count = concurrentMap.m[name]
-	concurrentMap.Unlock()
-	return count
-}
-
-func (concurrentMap *concurrentMap) getMap() map[string]int {
-	return concurrentMap.m
 }
