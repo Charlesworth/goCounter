1c1ea75e1e35d058ff1a3ba8c0da0da03d188f12
Charlesworth <cochrane26@gmail.com>
2015-10-16 01:49:57 +0100

    started implementing saveMap functionality

--- concurrentMap/saveMap.go
+++ concurrentMap/saveMap.go
@@ -2,7 +2,9 @@ package concurrentMap
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
+	"os"
 )
 
 func (concurrentMap *Map) SaveEveryInterval() {
@@ -19,6 +21,10 @@ func (concurrentMap *Map) Save() {
 
 }
 
+func Load() {
+
+}
+
 func mapToJson(m map[string]int) ([]byte, error) {
 	json, err := json.Marshal(m)
 	if err != nil {
@@ -32,6 +38,47 @@ func saveByteToFile(data []byte, fileName string) error {
 	return err
 }
 
-func Load() {
+// func SaveMap(m map[string]int, fileLocation string) error {
+// 	jsonMapByte, err := json.Marshal(m)
+// 	if err != nil {
+// 		return errors.New("SaveMap() failed to marshal map to JSON with error: " + err.Error())
+// 	}
+// 	err = ioutil.WriteFile(fileLocation, jsonMapByte, 0644)
+// 	if err != nil {
+// 		return errors.New("SaveMap() failed to write JSON map to file: " + err.Error())
+// 	}
+// 	return nil
+// }
 
+func fileExists(fileName string) bool {
+	if _, err := os.Stat(fileName); err == nil {
+		return true
+	}
+	return false
 }
+
+func jsonToMap(jsonByte []byte) (map[string]int, error) {
+	outputMap := make(map[string]int)
+	err := json.Unmarshal(jsonByte, &outputMap)
+	if err != nil {
+		return nil, errors.New("LoadMap() failed to unmarshal mapSaveFile JSON with error: " + err.Error())
+	}
+	return outputMap, nil
+}
+
+// func LoadMap(fileLocation string) (map[string]int, error) {
+// 	if _, err := os.Stat(fileLocation); os.IsNotExist(err) {
+// 		return nil, nil
+// 	}
+//
+// 	jsonMapByte, err := ioutil.ReadFile(fileLocation)
+// 	if err != nil {
+// 		return nil, errors.New("LoadMap() failed to read mapSaveFile: " + err.Error())
+// 	}
+// 	outputMap := make(map[string]int)
+// 	err = json.Unmarshal(jsonMapByte, &outputMap)
+// 	if err != nil {
+// 		return nil, errors.New("LoadMap() failed to unmarshal mapSaveFile JSON: " + err.Error())
+// 	}
+// 	return outputMap, nil
+// }
--- concurrentMap/saveMap_test.go
+++ concurrentMap/saveMap_test.go
@@ -1,6 +1,8 @@
 package concurrentMap
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -8,8 +10,54 @@ func TestMapToJson(t *testing.T) {
 	testMap := make(map[string]int)
 	testMap["element1"] = 123
 
-	testJson, _ := mapToJson(testMap)
-	if string(testJson) != "{\"element1\":123}" {
-		t.Error(string(testJson))
+	testJSON, _ := mapToJson(testMap)
+	if string(testJSON) != "{\"element1\":123}" {
+		t.Error(string(testJSON))
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	if fileExists("nonExistantFile") {
+		t.Error("fileExists() returned true when no file didn't exist")
+	}
+
+	ioutil.WriteFile("testFile", []byte{}, 644)
+	defer os.Remove("testFile")
+	if !fileExists("testFile") {
+		t.Error("fileExists() returned false when file existed")
+	}
+}
+
+func TestSaveByteToFile(t *testing.T) {
+	err := saveByteToFile([]byte{}, "testFile")
+	defer os.Remove("testFile")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !fileExists("testFile") {
+		t.Error("saveByteToFile('testFile') did not produce a file 'testFile'")
+	}
+}
+
+func TestJsonToMap(t *testing.T) {
+	invalidMapJSON := []byte(`{asdfasfasdfs}`)
+	nilMap, err := jsonToMap(invalidMapJSON)
+	if err == nil {
+		t.Error("jsonToMap with invalid json input did not produce an error")
+	}
+
+	if nilMap != nil {
+		t.Error("jsonToMap with invalid json input did not produce an error")
+	}
+
+	validMapJSON := []byte(`{"page1":1,"page2":2}`)
+	validMap, err := jsonToMap(validMapJSON)
+	if err != nil {
+		t.Error("jsonToMap with valid json input produced an error:", err)
+	}
+
+	if (validMap["page1"] != 1) && (validMap["page2"] != 2) {
+		t.Error("jsonToMap with valid json did not return correctly initialised map with values")
 	}
 }
